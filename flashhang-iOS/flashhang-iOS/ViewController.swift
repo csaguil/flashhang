73913017d76ecca5074a2701e44775775d2e4438
Cristian Saguil <cristian.saguil@gmail.com>
2018-07-28 13:00:35 -0700

    added flashhang iOS repo

---
+++ flashhang-iOS/flashhang-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flashhang-iOS
+//
+//  Created by Cristian Saguil on 7/28/18.
+//  Copyright © 2018 Cristian Saguil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ flashhang-iOS/flashhang-iOSTests/flashhang_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  flashhang_iOSTests.swift
+//  flashhang-iOSTests
+//
+//  Created by Cristian Saguil on 7/28/18.
+//  Copyright © 2018 Cristian Saguil. All rights reserved.
+//
+
+import XCTest
+@testable import flashhang_iOS
+
+class flashhang_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
