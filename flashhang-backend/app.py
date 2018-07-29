34be5fc837b91d6fbabf19f35d6f2379ed69ad21
Omid Afshar <omidafshar@berkeley.edu>
2018-07-29 05:26:41 -0700

    Yuh

--- flashhang-backend/app.py
+++ flashhang-backend/app.py
@@ -45,6 +45,9 @@ def add_new_user():
     users_ref.update({
         uid: new_user
     })
+    users_ref.child(uid).set(
+        new_user
+    )
     return json.dumps({"status":"success"})
 
 @app.route('/loggedin')
@@ -81,7 +84,7 @@ def join_lobby(lobby_id):
 
     this_user_ref = users_ref.child(uid)
     this_user = this_user_ref.get()
-                                                                  
+
     this_user_active_lobbies = this_user["active_lobbies"] + [str(lobby_id)] if "active_lobbies" in \
                                                                               this_user else [str(lobby_id)]
     this_user_ref.update({
@@ -97,6 +100,7 @@ def join_lobby(lobby_id):
         this_lobby_ref.update({
             "current_members": {
                 uid: {
+                    "name": this_user["name"],
                     "preferences": this_user["preferences"],
                     "location": user_current_location
                 }
@@ -104,6 +108,7 @@ def join_lobby(lobby_id):
         })
     else:
         this_lobby_current_members[uid] = {
+            "name": this_user["name"],
             "preferences": this_user["preferences"],
             "location": user_current_location
         }
@@ -126,6 +131,140 @@ def lobby_view(lobby_id):
 list_of_cats = ["escapegames","amusementparks", "gokarts", "museums", "cafes","bars", "karaoke", "zoos","makerspaces", "festivals","paintball", "mini_golf", "bowling","spas"]
 preference_watch  = []
 options = []
+
+#############Event Brite###########################
+
+def getSeatGeekr(idealLocation,highPrice,numberGoing, avgDist, radFactor,list_of_preferences,lobby_id):
+    global preference_watch
+    global options
+    client_id = '&client_id=MTI0NDczNjJ8MTUzMjgxMzA1OC43NA'
+    secret_code = '&client_secret=495e3c53fc8f2e4bb9a930d8eeab34d7e6ab3d2d98c6c5dde3e2dc14205532e9'
+    events_endpoint = 'https://api.seatgeek.com/2/events?'
+
+    dist = round(avgDist*radFactor)
+    dist = ceil(avgDist/2)
+    if(dist< 1):
+        dist = 1
+
+    lat = 'lat=' + str(idealLocation[0])
+    lon = '&lon=' + str(idealLocation[1])
+    radrange = '&range=' + str(dist) + 'mi'
+
+    currentDateTime = datetime.now() +  timedelta(hours=12)
+    endDateTime = currentDateTime + timedelta(hours=10)
+    currentDateTime.isoformat()
+    endDateTime.isoformat()
+
+    # currentDateTime = currentDateTime.isoformat()
+    # endDateTime = endDateTime.isoformat()
+
+
+    sgAvailable = '&listing_count.gte=' + str(numberGoing)
+    timeRange = '&datetime_local.gte='  + str(currentDateTime) + '&datetime_local.lte='  + str(endDateTime)
+    topPrice = '&average_price.lte=' + str(highPrice)
+
+    search = events_endpoint + lat + lon + radrange + sgAvailable + timeRange + topPrice + client_id + secret_code
+    sgList = requests.get(search)
+    JSONsgList = sgList.json()
+
+    if sgList.status_code == 200:
+        JSONsgList = sgList.json()
+
+        option_append = []
+        for event in JSONsgList["events"]:
+            option = {}
+            option['hang_option'] = 'seatgeek'
+            option["title"] = event["title"]
+            option["url"] = event["url"]
+            option["image"] = event["performers"]["images"]["huge"]
+            print(event["datetime_tbd"])
+            if event["datetime_tbd"] == False:
+                option["time"] = event["datetime_local"]
+                pass
+            if 'name' in event["venue"]:
+                option["address"] = event["venue"]["name"]
+            elif address in event["venue"]:
+                option["address"] = event["venue"]["address"]
+            elif slug in event["venue"]:
+                option["address"] = event["venue"]["slug"]
+            else:
+                print('no venue made available');
+            option_append.append(option)
+        options = options + option_append
+        preference_watch.append('1')
+
+        if len(preference_watch) == len(list_of_preferences) + 2:
+            preference_watch = []
+            make_a_choice(lobby_id)
+    else:
+        return request
+
+
+
+def getEventBrite(idealLocation, avgDist, radFactor, list_of_preferences,lobby_id):
+    global preference_watch
+    global options
+    base_string = 'https://www.eventbriteapi.com/v3/events/search/?'
+    lat = 'location.latitude='+str(idealLocation[0])
+    lon = '&location.longitude='+str(idealLocation[1])
+    dist = round(avgDist*radFactor)
+    if(dist< 1):
+        dist = 1
+
+    radrange = '&location.within='+str(dist)+'mi'
+
+
+
+    currentDateTime = datetime.now()
+    endDateTime = currentDateTime + timedelta(hours=3)
+
+    currentDateTime = currentDateTime.isoformat()[:-7]
+    endDateTime = endDateTime.isoformat()[:-7]
+
+
+    startTime = '&start_date.range_start='+str(currentDateTime)
+    endTime = '&start_date.range_end='+str(endDateTime)
+
+    search_string = base_string + lat + lon + radrange
+    # search_string = base_string + lat + lon + radrange + startTime + endTime
+    response = requests.get(search_string, headers = {"Authorization": "Bearer 5N4OSJMOEQBKAGX7HQSO"},verify = True)
+
+    JSONebList = response.json()
+
+    num_events = min(len(JSONebList['events']),15)
+
+    events = []
+    for n in range(0, num_events):
+        vid = JSONebList['events'][n]['venue_id']
+        response2 = requests.get('https://www.eventbriteapi.com/v3/venues/'+str(vid), headers = {"Authorization": "Bearer 5N4OSJMOEQBKAGX7HQSO"},verify = True)
+        JSONvid = response2.json()
+        address = str(JSONvid['address']['address_1'])+ ", " + str(JSONvid['address']['city']) + ", " + str(JSONvid['address']['region']) + " " + str(JSONvid['address']['postal_code'])
+        event = {}
+        event["name"] = JSONebList['events'][n]['name']['text']
+        event["address"] = address
+        event["url"] = JSONebList['events'][n]['url']
+        event["time"] = JSONebList['events'][n]['start']['local']
+        event["image"] = JSONebList['events'][n]['original']['url'] if 'original' in JSONebList['events'][n] and 'url' in JSONebList['events'][n]['original'] else 'https://www.tripsavvy.com/thmb/PdYn4eugR0zuP-oYGz00X9AYrvQ=/4037x2702/filters:fill(auto,1)/the-new-de-young-museum-in-golden-gate-park-148524473-591344515f9b5864703b6940.jpg'
+        events.append(event)
+
+    options = options + events
+    preference_watch.append('1')
+
+    if len(preference_watch) == len(list_of_preferences) + 2:
+        preference_watch = []
+        make_a_choice(lobby_id)
+
+
+    return events
+
+
+
+
+
+
+
+
+
 
 ##############find ideal location######################
 def lat_lon_miles(coord1, coord2):
@@ -177,7 +316,7 @@ def getIdealLocation(coordList):
 
 
 ################### SEATGEEK API ##################
-def getSeatGeek(idealLocation,highPrice,numberGoing, avgDist, list_of_preferences):
+def getSeatGeek(idealLocation,highPrice,numberGoing, avgDist, list_of_preferences,lobby_id):
     global preference_watch
     global options
 
@@ -229,22 +368,23 @@ def getSeatGeek(idealLocation,highPrice,numberGoing, avgDist, list_of_preference
         options = options + option_append
         preference_watch.append('1')
 
-        if len(preference_watch) == len(list_of_preferences) + 1:
+        if len(preference_watch) == len(list_of_preferences) + 2:
             preference_watch = []
-            make_a_choice()
+            make_a_choice(lobby_id)
     else:
         return request
 
 
 
 #################YELP API#################
-def run_yelp_graph_query(query, headers, list_of_preferences): # A simple function to use requests.post to make the API call. Note the json= section.
+def run_yelp_graph_query(query, headers, list_of_preferences,lobby_id): # A simple function to use requests.post to make the API call. Note the json= section.
     global preference_watch
     global options
     request = requests.post('https://api.yelp.com/v3/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         yelp_return = request.json()
-
+        if yelp_return == None:
+            return({"status": "error"})
         option_append = []
         for option in yelp_return['data']['search']['business']:
             option["hang_option"] = 'yelp'
@@ -252,9 +392,9 @@ def run_yelp_graph_query(query, headers, list_of_preferences): # A simple functi
         options = options + option_append
 
         preference_watch.append('1')
-        if len(preference_watch) == len(list_of_preferences) + 1:
+        if len(preference_watch) == len(list_of_preferences) + 2:
             preference_watch = []
-            make_a_choice()
+            make_a_choice(lobby_id)
     else:
         return request
         # raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
@@ -262,7 +402,7 @@ def run_yelp_graph_query(query, headers, list_of_preferences): # A simple functi
 
 
 #intergrate with yelp api
-def get_yelp_choices(list_of_preferences, ideal_location):
+def get_yelp_choices(list_of_preferences, ideal_location, lobby_id):
     #TODO CHANGE THIS
     list_of_preferences = list_of_cats
     latitude = 0
@@ -286,8 +426,8 @@ def get_yelp_choices(list_of_preferences, ideal_location):
                 business {
                     name
                     rating
-                    review_count
                     display_phone
+                    photos
                     reviews {
                         id
                     }
@@ -301,7 +441,7 @@ def get_yelp_choices(list_of_preferences, ideal_location):
             }
         }
         """
-        run_yelp_graph_query(query_with_locations,headers,list_of_preferences)
+        run_yelp_graph_query(query_with_locations,headers,list_of_preferences,lobby_id)
 
 
 def test():
@@ -309,41 +449,47 @@ def test():
     ideal_location.append(37.780126)
     ideal_location.append(-122.410536)
     getSeatGeek(ideal_location,1000,100, 100,list_of_cats)
+    # getSeatGeekr(ideal_location,1000,100, 100, 0.5,list_of_cats)
     get_yelp_choices('things',ideal_location)
+    getEventBrite(ideal_location, 100, 0.5, list_of_cats)
 
 
-def begin_compromise(lobby):
+def begin_compromise(lobby,lobby_id):
     list_of_preferences = []
     list_of_user_coords = []
-    for uuid in  lobby["current_members"]:
+    for uuid in lobby["current_members"]:
         for preference in lobby["current_members"][uuid]["preferences"]:
             list_of_preferences.append(preference)
         list_of_user_coords.append(lobby["current_members"][uuid]["location"])
 
     #get ideal location
-    location_return = getIdealLocation(coordList)
+    print(list_of_user_coords)
+    location_return = getIdealLocation(list_of_user_coords)
     ideal_location = location_return[0]
     radius = location_return[1]
     #Remove this
-    ideal_location = [37.780126, -122.410536]
-    getSeatGeek(ideal_location,1000,100, 100,list_of_cats)
-    get_yelp_choices(list(set(list_of_preferences)), ideal_location)
+    # ideal_location = [37.780126, -122.410536]
+    print(ideal_location)
+    getSeatGeek(ideal_location,1000,100, 100,list_of_cats,lobby_id)
+    get_yelp_choices(list(set(list_of_preferences)), ideal_location,lobby_id)
+    getEventBrite(ideal_location, 100, 0.5, list_of_cats,lobby_id)
 
 
-def update_lobby_on_firebase(choices):
+def update_lobby_on_firebase(choices,lobby_id):
     this_lobby_ref = lobby_ref.child(lobby_id)
-    
+
     this_lobby_ref.update({
             "choices": {
                 "0": choices[0],
                 "1": choices[1],
-                "2": choices[3]
-        }
+                "2": choices[2]
+        },
+        "state": "choice"
     })
-    return {"status":"success"}
+    return json.dumps({"status":"success"})
 
 
-def make_a_choice():
+def make_a_choice(lobby_id):
     global options
     choices = []
     choice_1 = randint(0, len(options))
@@ -358,7 +504,8 @@ def make_a_choice():
     choices.append(options[choice_1])
     choices.append(options[choice_2])
     choices.append(options[choice_3])
-    # update_lobby_on_firebase(choices)
+    print(choices)
+    update_lobby_on_firebase(choices,lobby_id)
     #write --> to firebase
     options = []
 
@@ -372,12 +519,11 @@ def make_choice(lobby_id):
     # make choice -> post to firebase
     #get lobby object with call back ->
     # get a set of preferences
+    # lobby_id = response.get_json()["lobby_id"]
     snapshot = lobby_ref.child(lobby_id).get()
     if snapshot is None:
         return json.dumps({"error": "User not found"})
     else:
-        lobby = json.dumps(snapshot)
-        #begin_compromise(lobby)
-        return {"status":"success"}
-        
-
+        # lobby = json.dumps(snapshot)
+        begin_compromise(snapshot, lobby_id)
+        return json.dumps({"status":"success"})
